462845e4e7b2088acb1ff3b0005a8daace1f0c21
osman-boy <osulejmanov3@gmail.com>
2022-02-18 21:02:32 +0600

    Add Game Repository

--- app/src/main/java/com/android/composition/domain/entity/GameResult.kt
+++ app/src/main/java/com/android/composition/domain/entity/GameResult.kt
@@ -2,7 +2,6 @@ package com.android.composition.domain.entity
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import ru.sumin.composition.domain.entity.GameSettings
 
 @Parcelize
 data class GameResult(
