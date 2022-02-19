9f0b7e56fbbf024bed727083bab874865b50614c
osman-boy <osulejmanov3@gmail.com>
2022-02-19 14:52:27 +0600

    Refactoring

--- app/src/main/java/com/android/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/android/composition/presentation/GameFragment.kt
@@ -1,18 +1,14 @@
 package com.android.composition.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.android.composition.databinding.FragmentGameBinding
-import com.android.composition.domain.entity.GameResult
 
 
 class GameFragment : Fragment() {
